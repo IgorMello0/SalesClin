c05331dad860eeca201c94744d43394047f07b8f
Igor de Mello <igor.mello@santacasa.sjrp>
2026-05-05 18:16:37 -0300

    build: configurando projeto para deploy na Vercel

--- server/index.ts
+++ server/index.ts
@@ -70,9 +70,13 @@ app.use((err: any, _req: express.Request, res: express.Response, _next: express.
 })
 
 const port = process.env.PORT || 4000
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`[server] listening on http://localhost:${port}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`[server] listening on http://localhost:${port}`)
+  })
+}
+
+export default app
 
 
